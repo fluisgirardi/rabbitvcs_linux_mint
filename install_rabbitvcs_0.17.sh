f917bd5fb522e4c01ae775246ad0d785327c5999
Fabio Luis Girardi <fabio@pascalscada.com>
2018-07-23 16:14:13 -0300

    Encontrada outra dependencia

--- install_rabbitvcs_0.17.sh
+++ install_rabbitvcs_0.17.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install git subversion nemo-python python-svn python3-svn python-simplejson python3-simplejson meld nemo nemo-python python-dbus python-dulwich python3-dulwich
+sudo apt install git subversion nemo-python python-svn python3-svn python-simplejson python3-simplejson meld nemo nemo-python python-dbus python-dulwich python3-dulwich python-git python3-git
 cd /tmp
 git clone https://github.com/rabbitvcs/rabbitvcs.git
 cd rabbitvcs
