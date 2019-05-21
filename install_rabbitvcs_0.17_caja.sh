52ea7fbdbe4d97975332ed521ca2bcdc703b7f8b
Fabio Luis Girardi <fabio@pascalscada.com>
2019-05-20 22:21:35 -0300

    RabbitVCS Install Script for Caja
    Adicionado script para instalar o RabbitVCS no caja.

---
+++ install_rabbitvcs_0.17_caja.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt install git subversion python-svn python-simplejson meld caja python-caja python-dbus python-dulwich python-git python-tk gir1.2-gtkspell3-3.0
+cd /tmp
+git clone https://github.com/rabbitvcs/rabbitvcs.git
+cd rabbitvcs
+sudo python setup.py install --install-layout=deb
+if [ ! -d /usr/share/caja-python/extensions ]; then
+  sudo mdkir /usr/share/caja-python/extensions;
+fi
+sudo cp clients/caja/RabbitVCS.py /usr/share/caja-python/extensions
+
+caja -q
+pgrep -f service.py | xargs kill
+nohup caja > /dev/null &
